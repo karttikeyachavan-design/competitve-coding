defa16cb51f70a0730fa5eaaff195f7c3ab9fbc0
karttikeyachavan-design <karttikeyachavan@gmail.com>
2026-08-14 23:35:52 +0530

    Create 61A_Ultra-Fast-Mathematician.java

---
+++ Codeforces/61A_Ultra-Fast-Mathematician.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+ 
+public class Main {
+    public static void main(String[] args) {
+ 
+        Scanner sc = new Scanner(System.in);
+ 
+        String s1 = sc.next();
+        String s2 = sc.next();
+ 
+        StringBuilder result = new StringBuilder();
+ 
+        for (int i = 0; i < s1.length(); i++) {
+ 
+            if ( s1.charAt(i) == s2.charAt(i)){
+ 
+                result.append('0');
+ 
+            } else {
+ 
+                result.append('1');
+            }
+        }
+ 
+        System.out.println(result);
+ 
+        sc.close();
+    }
+}
