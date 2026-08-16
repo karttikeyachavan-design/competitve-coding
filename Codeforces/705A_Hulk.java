e1c6f599dc547909ce9fe14727ff57c7ce9b4174
karttikeyachavan-design <karttikeyachavan@gmail.com>
2026-08-16 23:44:13 +0530

    Create 705A_Hulk.java

---
+++ Codeforces/705A_Hulk.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+ 
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        StringBuilder result = new StringBuilder();
+ 
+        for (int i = 1; i <= n; i++) {
+            if (i % 2 != 0) {
+                result.append("I hate");
+            } else {
+                result.append("I love");
+            }
+ 
+            if (i == n) {
+                result.append(" it");
+            } else {
+                result.append(" that ");
+            }
+        }
+ 
+        System.out.println(result.toString());
+    }
+}
