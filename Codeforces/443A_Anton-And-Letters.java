a1f5904642513926aacf4767fafff705baa672b4
karttikeyachavan-design <karttikeyachavan@gmail.com>
2026-08-24 22:04:20 +0530

    Create 443A_Anton-And-Letters.java

---
+++ Codeforces/443A_Anton-And-Letters.java
@@ -0,0 +1,26 @@
+import java.util.HashSet;
+import java.util.Scanner;
+ 
+public class Main {
+    public static void main(String[] args) {
+ 
+        Scanner sc = new Scanner(System.in);
+ 
+        String s = sc.nextLine();
+ 
+        HashSet<Character> set = new HashSet<>();
+ 
+        for (int i = 0; i < s.length(); i++) {
+ 
+            char ch = s.charAt(i);
+ 
+            if (Character.isLowerCase(ch)) {
+                set.add(ch);
+            }
+        }
+ 
+        System.out.println(set.size());
+ 
+        sc.close();
+    }
+}
