f13de2260f4a1943bcd6578455336d1950862ea9
karttikeyachavan-design <karttikeyachavan@gmail.com>
2026-08-27 19:56:32 +0530

    Create 580A_Kefa-and-First-Steps.java

---
+++ Codeforces/580A_Kefa-and-First-Steps.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+        }
+        
+        int maxLen = 1;     
+        int currentLen = 1; 
+        
+        for (int i = 1; i < n; i++) {
+            if (a[i] >= a[i - 1]) {
+                currentLen++;
+            } else {
+                if (currentLen > maxLen) {
+                    maxLen = currentLen;
+                }
+                currentLen = 1; 
+            }
+        }
+        
+        if (currentLen > maxLen) {
+            maxLen = currentLen;
+        }
+        
+        System.out.println(maxLen);
+        sc.close();
+    }
+}
