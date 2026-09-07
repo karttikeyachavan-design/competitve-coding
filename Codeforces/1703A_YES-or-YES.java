4d3ba51affc47d60d28212ad62333d88effbe5e9
karttikeyachavan-design <karttikeyachavan@gmail.com>
2026-09-07 23:03:02 +0530

    Create 1703A_YES-or-YES.java

---
+++ Codeforces/1703A_YES-or-YES.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+ 
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int t = sc.nextInt();
+        
+        for (int i = 0; i < t; i++) {
+            String s = sc.next();
+            
+            if (s.equalsIgnoreCase("YES")) {
+                System.out.println("YES");
+            } else {
+                System.out.println("NO");
+            }
+        }
+        
+        sc.close();
+    }
+}
