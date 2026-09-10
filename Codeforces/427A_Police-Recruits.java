c1f81a7e411c9d61454a7aa8221d90030ddc2ff1
karttikeyachavan-design <karttikeyachavan@gmail.com>
2026-09-10 23:53:17 +0530

    Create 427A_Police-Recruits.java

---
+++ Codeforces/427A_Police-Recruits.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+ 
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        int n = scanner.nextInt();
+        
+        int availableOfficers = 0;
+        int untreatedCrimes = 0;
+        
+        for (int i = 0; i < n; i++) {
+            int event = scanner.nextInt();
+            
+            if (event == -1) {
+                if (availableOfficers > 0) {
+                    availableOfficers--; 
+                } else {
+                    untreatedCrimes++; 
+                }
+            } else {
+                availableOfficers += event;
+            }
+        }
+        
+        System.out.println(untreatedCrimes);
+        
+        scanner.close();
+    }
+}
